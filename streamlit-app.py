489f539cea8a9d76ab55331be5e94f65204aca4d
Franck Mairot <franck.mairot@gmail.com>
2023-05-01 15:44:27 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import itertools
 import streamlit as st
 import base64
+import st_dataframe as stdf
 
 # Titre de l'application Streamlit
 st.title("Sémantique Web : Le meilleur outil pour vous brief")
@@ -17,8 +18,14 @@ api_key_1 = st.sidebar.text_input("Clé API 1")
 api_key_2 = st.sidebar.text_input("Clé API 2")
 api_key_3 = st.sidebar.text_input("Clé API 3")
 
-result_data = []
+# Créer un DataFrame vide avec les colonnes souhaitées
+columns = ["keyword", "Nb de mots suggérés", "Volume de recherches", "headings_thruuu", "Mots / concepts importants à développer"]
+data = pd.DataFrame(columns=columns)
 
+# Afficher le DataFrame éditable
+data = stdf.edit_dataframe(data)
+
+# Si le fichier CSV est téléchargé et que les clés API sont fournies
 if uploaded_file and api_key_1 and api_key_2 and api_key_3:
     # Remplacez les clés API par vos propres clés
     api_keys = [api_key_1, api_key_2, api_key_3]
@@ -28,6 +35,11 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
     # Lire le fichier CSV avec l'encodage UTF-8
     data = pd.read_csv(uploaded_file, encoding="utf-8")
+
+# Si des données sont présentes dans le DataFrame
+if not data.empty:
+    # Création d'un DataFrame pour stocker les résultats
+    result_data = []
 
     # Traitement de chaque ligne du fichier CSV
     for index, row in data.iterrows():
@@ -43,35 +55,32 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
         # Création du prompt en combinant les variables et la phrase fixe
         prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief :{headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure celons les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
 
-        messages = [
-            {"role": "system", "content": prompt_text},
-        ]
+    messages = [
+        {"role": "system", "content": prompt_text},
+    ]
 
-        message = "User : "
+    message = "User : "
 
-        if message:
-            messages.append(
-                {"role": "user", "content": message},
-            )
-            chat = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo", messages=messages
-            )
-            reply = chat.choices[0].message.content
-            
-            # Ajout des résultats dans le DataFrame
-            result_data.append({
-                "keyword": keyword,
-                "Nb_de_mots_suggérés": nombre_de_mots,
-                "volume de recherche": volume_recherche,
-                "prompt": prompt_text,
-                "Structure Hn suggerée": reply
-            })
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        
+        # Ajout des résultats dans le DataFrame
+        result_data.append({
+            "keyword": keyword,
+            "Nb_de_mots_suggérés": nombre_de_mots,
+            "volume de recherche": volume_recherche,
+            "prompt": prompt_text,
+            "Structure Hn suggerée": reply
+        })
+
 # Conversion de la liste de résultats en DataFrame
-try:
-    df = pd.DataFrame(result_data)
-except Exception as e:
-    st.write("Erreur lors de la création du DataFrame :")
-    st.write(e)
+df = pd.DataFrame(result_data)
 
 # Affichage du DataFrame dans l'application Streamlit
 st.write(df)
@@ -81,3 +90,4 @@ csv = df.to_csv(index=False, encoding="utf-8").encode()
 b64 = base64.b64encode(csv).decode()
 href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats en format CSV</a>'
 st.markdown(href, unsafe_allow_html=True)
+
