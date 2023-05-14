1d69715ec2c68dfb9b357b65faaa2451cd016af6
Franck Mairot <franck.mairot@gmail.com>
2023-05-14 23:52:13 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -23,19 +23,20 @@ api_key_1 = st.text_input("Clé API 1")
 api_key_2 = st.text_input("Clé API 2")
 api_key_3 = st.text_input("Clé API 3")
 
-# Demande du prompt et du style de rédaction
-prompt_option = st.selectbox(
-    "Choisissez votre prompt",
-    ("Semantique IA", "Géoloc IA", "Business IA")
-)
-
-writing_style = st.text_input("Quel style de rédaction souhaitez-vous ?")
-
 # Deuxième itération pour générer un appel à OpenAI GPT-4 avec un prompt de rédaction
 st.header("Génération de contenu avec OpenAI GPT-4")
 
 # Demander à l'utilisateur s'il souhaite générer du contenu supplémentaire
 generate_content = st.checkbox("Générer du contenu supplémentaire")
+
+# Demander le style de rédaction uniquement si la case est cochée
+if generate_content:
+    writing_style = st.selectbox(
+        "Quel style de rédaction souhaitez-vous ?",
+        ("Pas de choix", "Prompt 1", "Prompt 2", "Prompt 3")
+    )
+else:
+    writing_style = "Pas de choix"
 
 # Création d'une liste pour stocker les résultats
 result_data = []
@@ -63,8 +64,7 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
         # Création du prompt en fonction de l'option choisie par l'utilisateur
         if prompt_option == "Semantique IA":
-            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief : {headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure selon les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
-
+            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief :{headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure selon les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
         elif prompt_option == "Géoloc IA":
             prompt_text = f"Veuillez insérer ici votre propre texte de prompt pour 'Géoloc IA'."
 
@@ -108,7 +108,7 @@ if result_data:
     st.markdown(href, unsafe_allow_html=True)
 
     # Deuxième itération pour générer un appel à OpenAI GPT-4 avec un prompt de rédaction
-    if generate_content:
+    if generate_content and writing_style != "Pas de choix":
         st.header("Génération de contenu avec OpenAI GPT-4")
 
         # Création d'une nouvelle liste pour stocker les résultats de la deuxième itération
@@ -133,13 +133,14 @@ if result_data:
                 )
                 reply_2 = chat_2.choices[0].message.content
 
-                #Ajout des résultats dans la liste result_data_2
+                # Ajout des résultats dans la liste result_data_2
                 result_data_2.append({
-                "Structure Hn": structure_hn,
-                "Article généré": reply_2
+                    "Structure Hn": structure_hn,
+                    "Article généré": reply_2
                 })
+
         # Conversion de la liste de résultats en DataFrame
-        df_2 = pd.DataFrame(result_data_2)
+        df= pd.DataFrame(result_data_2)
 
         # Affichage du DataFrame de la deuxième itération dans l'application Streamlit
         st.write(df_2)
@@ -152,4 +153,8 @@ if result_data:
 
 else:
     st.warning("Aucune donnée à afficher. Veuillez fournir un fichier CSV et les clés API pour continuer.")
+
+else:
+st.warning("Veuillez fournir un fichier CSV et les clés API pour continuer.")
+
 
