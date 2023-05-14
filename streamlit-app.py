0000c4b2601db4ac8a55f915468c44dce9eae7a7
Franck Mairot <franck.mairot@gmail.com>
2023-05-15 00:30:28 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -28,7 +28,13 @@ prompt_option = st.selectbox(
     ("Semantique IA", "Géoloc IA", "Business IA")
 )
 
-writing_style = st.text_input("Quel style de rédaction souhaitez-vous ?")
+writing_style_option = st.selectbox(
+    "Quel style de rédaction souhaitez-vous ?",
+    ("Style 1", "Style 2", "Style 3")
+)
+
+# Création d'un DataFrame pour stocker les résultats
+result_data = []
 
 # Vérification si le fichier et les clés API sont fournis
 if uploaded_file and api_key_1 and api_key_2 and api_key_3:
@@ -41,11 +47,9 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
     # Lire le fichier CSV avec l'encodage UTF-8
     data = pd.read_csv(uploaded_file, encoding="utf-8")
 
-    # Création d'un DataFrame pour stocker les résultats
-    result_data = []
-
     # Traitement de chaque ligne du fichier CSV
     for index, row in data.iterrows():
+        # Extraction des données de la ligne actuelle
         keyword = row["keyword"]
         nombre_de_mots = row["Nb de mots suggérés"]
         volume_recherche = row["Volume de recherches"]
@@ -57,22 +61,30 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
         # Création du prompt en fonction de l'option choisie par l'utilisateur
         if prompt_option == "Semantique IA":
-            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief : {headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure selon les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
+            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief : {headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rérédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure selon les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
         elif prompt_option == "Géoloc IA":
             prompt_text = f"Veuillez insérer ici votre propre texte de prompt pour 'Géoloc IA'."
         elif prompt_option == "Business IA":
             prompt_text = f"Veuillez insérer ici votre propre texte de prompt pour 'Business IA'."
+    # Choisir le prompt en fonction du style de rédaction choisi
+    if writing_style_option == "Style 1":
+        prompt_text += " (Style 1)"
+    elif writing_style_option == "Style 2":
+        prompt_text += " (Style 2)"
+    elif writing_style_option == "Style 3":
+        prompt_text += " (Style 3)"
 
-        messages = [
-            {"role": "system", "content": prompt_text},
-        ]
+    messages = [
+        {"role": "system", "content": prompt_text},
+    ]
 
-        message = "User:"
+    message = "User: "
 
-        if message:
-            messages.append(
-                {"role": "user", "content": message},
-            )
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        try:
             chat = openai.ChatCompletion.create(
                 model="gpt-4", messages=messages
             )
@@ -85,15 +97,17 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
                 "volume de recherche": volume_recherche,
                 "Structure Hn suggerée": reply
             })
+        except Exception as e:
+            st.error(f"Erreur lors de la génération du contenu pour la ligne {index + 1}: {str(e)}")
 
-    # Conversion de la liste de résultats en DataFrame
-    df = pd.DataFrame(result_data)
+# Conversion de la liste de résultats en DataFrame
+df = pd.DataFrame(result_data)
 
-    # Affichage du DataFrame dans l'application Streamlit
-    st.write(df)
+# Affichage du DataFrame dans l'application Streamlit
+st.write(df)
 
-    # Bouton pour télécharger le fichier CSV résultant
-    csv = df.to_csv(index=False, encoding="utf-8").encode()
-    b64 = base64.b64encode(csv).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats en format CSV</a>'
-    st.markdown(href, unsafe_allow_html=True)
+# Bouton pour télécharger le fichier CSV résultant
+csv = df.to_csv(index=False, encoding="utf-8").encode()
+b64 = base64.b64encode(csv).decode()
+href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats en format CSV</a>'
+st.markdown(href, unsafe_allow_html=True)
