fece519787e876eb526f5d84297848a12905e47b
Franck Mairot <franck.mairot@gmail.com>
2023-05-14 23:13:24 +0200

    amélioration ux/ui

--- streamlit-app.py
+++ streamlit-app.py
@@ -23,14 +23,13 @@ api_key_1 = st.text_input("Clé API 1")
 api_key_2 = st.text_input("Clé API 2")
 api_key_3 = st.text_input("Clé API 3")
 
-# Ajout d'une option pour l'utilisateur de choisir le prompt
+# Ajout d'une option pour l'utilisateur de choisir le prompt et de générer du contenu supplémentaire
 prompt_option = st.selectbox(
     "Choisissez votre prompt",
     ("Semantique IA", "Géoloc IA", "Business IA")
 )
 
-# Création d'un DataFrame pour stocker les résultats
-result_data = []
+generate_content = st.checkbox("Générer du contenu supplémentaire")
 
 if uploaded_file and api_key_1 and api_key_2 and api_key_3:
     # Remplacez les clés API par vos propres clés
@@ -41,6 +40,9 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
     # Lire le fichier CSV avec l'encodage UTF-8
     data = pd.read_csv(uploaded_file, encoding="utf-8")
+
+    # Création d'un DataFrame pour stocker les résultats
+    result_data = []
 
     # Traitement de chaque ligne du fichier CSV
     for index, row in data.iterrows():
@@ -55,11 +57,13 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
         # Création du prompt en fonction de l'option choisie par l'utilisateur
         if prompt_option == "Semantique IA":
-            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief :{headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure celons les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
+            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief :{headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence
+
         elif prompt_option == "Géoloc IA":
-            prompt_text = "Veuillez insérer ici votre propre texte de prompt pour l'option 2."
-        else:
-            prompt_text = "Veuillez insérer ici votre propre texte de prompt pour l'option 3."
+            prompt_text = "Veuillez insérer ici votre propre texte de prompt pour l'Géoloc IA'."
+
+        elif prompt_option == "Business IA":
+            prompt_text = "Veuillez insérer ici votre propre texte de prompt pour 'Business IA'."
 
         messages = [
             {"role": "system", "content": prompt_text},
@@ -90,19 +94,10 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
     # Affichage du DataFrame dans l'application Streamlit
     st.write(df)
 
-    # Bouton pour télécharger le fichier CSV résultant
-    csv = df.to_csv(index=False, encoding="utf-8").encode()
-    b64 = base64.b64encode(csv).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats en format CSV</a>'
-    st.markdown(href, unsafe_allow_html=True)
-
     # Deuxième itération pour générer un appel à OpenAI GPT-4 avec un prompt de rédaction
-    st.header("Génération de contenu avec OpenAI GPT-4")
-
-    # Demander à l'utilisateur s'il souhaite générer du contenu supplémentaire
-    generate_content = st.checkbox("Générer du contenu supplémentaire")
-
     if generate_content:
+        st.header("Génération de contenu avec OpenAI GPT-4")
+
         # Création d'un DataFrame pour stocker les résultats de la deuxième itération
         result_data_2 = []
 
@@ -134,13 +129,20 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
         # Conversion de la liste de résultats en DataFrame
         df_2 = pd.DataFrame(result_data_2)
 
-        # Affichage du DataFrame dans l'application Streamlit
+        # Affichage du DataFrame de la deuxième itération dans l'application Streamlit
         st.write(df_2)
 
-        # Bouton pour télécharger le fichier CSV résultant
+    # Bouton pour télécharger le fichier CSV résultant de la première itération
+    csv = df.to_csv(index=False, encoding="utf-8").encode()
+    b64 = base64.b64encode(csv).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats de la première itération en format CSV</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+    # Bouton pour télécharger le fichier CSV résultant de la deuxième itération
+    if generate_content:
         csv_2 = df_2.to_csv(index=False, encoding="utf-8").encode()
         b64_2 = base64.b64encode(csv_2).decode()
         href_2 = f'<a href="data:file/csv;base64,{b64_2}" download="resultat_2.csv">Télécharger les résultats de la deuxième itération en format CSV</a>'
         st.markdown(href_2, unsafe_allow_html=True)
-
-
+    else:
+        st.warning("Veuillez fournir un fichier CSV et les clés API pour continuer.")
