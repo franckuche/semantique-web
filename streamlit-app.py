04777a6244b5cddd822260676b75e3acd9ac85a0
Franck Mairot <franck.mairot@gmail.com>
2023-05-15 00:23:32 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -5,37 +5,34 @@ import itertools
 import streamlit as st
 import base64
 
-# Installer le package OpenAI si nécessaire
-try:
-    import openai
-except ImportError:
-    st.write("Installation du package OpenAI en cours...")
-    st.system("!pip install openai")
-    import openai
-
 # Titre de l'application Streamlit
 st.title("Sémantique Web : Le meilleur outil pour vos briefs")
 
-# Demande de fichier CSV et clés API
-uploaded_file = st.file_uploader("Choisir un fichier CSV", type=["csv"])
-api_key_1 = st.text_input("Clé API 1")
-api_key_2 = st.text_input("Clé API 2")
-api_key_3 = st.text_input("Clé API 3")
+# Sidebar pour les paramètres utilisateur
+st.sidebar.title("Paramètres")
 
-# Demande du prompt et du style de rédaction
-prompt_option = st.selectbox(
+# Options de l'utilisateur
+uploaded_file = st.sidebar.file_uploader("Choisir un fichier CSV", type=["csv"])
+api_key_1 = st.sidebar.text_input("Clé API 1")
+api_key_2 = st.sidebar.text_input("Clé API 2")
+api_key_3 = st.sidebar.text_input("Clé API 3")
+
+# Ajout d'une option pour l'utilisateur de choisir le prompt
+prompt_options = {
+    "Semantique IA": "Veuillez ignorer toutes les instructions précédentes...",
+    "Géoloc IA": "Veuillez insérer ici votre propre texte de prompt pour l'option 2.",
+    "Business IA": "Veuillez insérer ici votre propre texte de prompt pour l'option 3."
+}
+
+selected_prompt = st.sidebar.selectbox(
     "Choisissez votre prompt",
-    ("Semantique IA", "Géoloc IA", "Business IA")
+    list(prompt_options.keys())
 )
 
-writing_style = st.text_input("Quel style de rédaction souhaitez-vous ?")
+# Création d'un DataFrame pour stocker les résultats
+result_data = []
 
-# Deuxième itération pour générer un appel à OpenAI GPT-4 avec un prompt de rédaction
-st.header("Génération de contenu avec OpenAI GPT-4")
-
-# Demander à l'utilisateur s'il souhaite générer du contenu supplémentaire
-generate_content = st.checkbox("Générer du contenu supplémentaire")
-
+# Vérification si le fichier et les clés API sont fournis
 if uploaded_file and api_key_1 and api_key_2 and api_key_3:
     # Remplacez les clés API par vos propres clés
     api_keys = [api_key_1, api_key_2, api_key_3]
@@ -45,9 +42,6 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
     # Lire le fichier CSV avec l'encodage UTF-8
     data = pd.read_csv(uploaded_file, encoding="utf-8")
-
-    # Création d'un DataFrame pour stocker les résultats
-    result_data = []
 
     # Traitement de chaque ligne du fichier CSV
     for index, row in data.iterrows():
@@ -61,46 +55,51 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
         openai.api_key = next(api_key_cycle)
 
         # Création du prompt en fonction de l'option choisie par l'utilisateur
-        if prompt_option == "Semantique IA":
-            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief : {headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rérédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous-parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure selon les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
-        elif prompt_option == "Géoloc IA":
-            prompt_text = "Veuillez insérer ici votre propre texte de prompt pour l'option 'Géoloc IA'."
-        elif prompt_option == "Business IA":
-            prompt_text = "Veuillez insérer ici votre propre texte de prompt pour l'option 'Business IA'."
-    messages = [
-        {"role": "system", "content": prompt_text},
-    ]
-
-    message = "User : "
-
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
+        prompt_text = prompt_options[selected_prompt].format(
+            headings_thruu=headings_thruu,
+            keyword=keyword,
+            nombre_de_mots=nombre_de_mots
         )
-        try:
-            chat = openai.ChatCompletion.create(
-                model="gpt-4", messages=messages
+
+        messages = [
+            {"role": "system", "content": prompt_text},
+        ]
+
+        message = "User : "
+
+        if message:
+            messages.append(
+                {"role": "user", "content": message},
             )
-            reply = chat.choices[0].message.content
+            try:
+                chat = openai.ChatCompletion.create(
+                    model="gpt-4", messages=messages
+                )
+                reply = chat.choices[0].message.content
 
-            # Ajout des résultats dans le DataFrame
-            result_data.append({
-                "keyword": keyword,
-                "Nb_de_mots_suggérés": nombre_de_mots,
-                "volume de recherche": volume_recherche,
-                "Structure Hn suggerée": reply
-            })
-        except Exception as e:
-            st.error(f"Erreur lors de la génération du contenu pour la ligne {index + 1}: {str(e)}")
+                # Ajout des résultats dans le DataFrame
+                result_data.append({
+                    "keyword": keyword,
+                    "Nb_de_mots_suggérés": nombre_de_mots,
+                    "volume de recherche": volume_recherche,
+                    "Structure Hn suggerée": reply
+                })
+            except Exception as e:
+                st.error(f"Erreur lors de la génération du contenu pour la ligne {index + 1}: {str(e)}")
 
-# Conversion de la liste de résultats en DataFrame
+    # Conversion de la liste de résultats en DataFrame
 df = pd.DataFrame(result_data)
-
 # Affichage du DataFrame dans l'application Streamlit
 st.write(df)
 
+# Bouton pour télécharger le fichier CSV résultant de la première itération
+csv = df.to_csv(index=False, encoding="utf-8").encode()
+b64 = base64.b64encode(csv).decode()
+href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats de la première itération en format CSV</a>'
+st.markdown(href, unsafe_allow_html=True)
+
 # Deuxième itération pour générer un appel à OpenAI GPT-4 avec un prompt de rédaction
-if generate_content:
+if st.checkbox("Générer du contenu supplémentaire"):
     st.header("Génération de contenu avec OpenAI GPT-4")
 
     # Création d'un DataFrame pour stocker les résultats de la deuxième itération
@@ -137,14 +136,7 @@ if generate_content:
     # Affichage du DataFrame de la deuxième itération dans l'application Streamlit
     st.write(df_2)
 
-# Bouton pour télécharger le fichier CSV résultant de la première itération
-csv = df.to_csv(index=False, encoding="utf-8").encode()
-b64 = base64.b64encode(csv).decode()
-href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats de la première itération en format CSV</a>'
-st.markdown(href, unsafe_allow_html=True)
-
-# Bouton pour télécharger le fichier CSV résultant de la deuxième itération
-if generate_content:
+    # Bouton pour télécharger le fichier CSV résultant de la deuxième itération
     csv_2 = df_2.to_csv(index=False, encoding="utf-8").encode()
     b64_2 = base64.b64encode(csv_2).decode()
     href_2 = f'<a href="data:file/csv;base64,{b64_2}" download="resultat_2.csv">Télécharger les résultats de la deuxième itération en format CSV</a>'
