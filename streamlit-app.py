ab667483c1fdfa0e8c35696c795923d5c52e3548
Franck Mairot <franck.mairot@gmail.com>
2023-05-14 23:01:55 +0200

    test 2 steps

--- streamlit-app.py
+++ streamlit-app.py
@@ -17,6 +17,7 @@ except ImportError:
 # Titre de l'application Streamlit
 st.title("Sémantique Web : Le meilleur outil pour vos briefs")
 
+# Demande de fichier CSV et clés API
 uploaded_file = st.file_uploader("Choisir un fichier CSV", type=["csv"])
 api_key_1 = st.text_input("Clé API 1")
 api_key_2 = st.text_input("Clé API 2")
@@ -54,7 +55,7 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
 
         # Création du prompt en fonction de l'option choisie par l'utilisateur
         if prompt_option == "Semantique IA":
-            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief :{headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez essayer d'inclure celons les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [videos], etc..."
+            prompt_text = f"Veuillez ignorer toutes les instructions précédentes. Tu es un expert en référencement SEO reconnu en France. Tu dois délivrer un brief de très haute qualité à tes rédacteurs. Voici quelques informations sur ce qu'est un bon brief en 2023, il faudra t'appuyer sur ces dernières pour ta proposition de brief :{headings_thruu}. En adaptant ton brief aux conseils ci-dessus, propose-moi un brief complet pour un texte sur {keyword} pour mon rédacteur en adaptant la longueur de ce dernier en fonction de la longueur du texte que je vais vous demander, en l'occurrence pour celui-ci j'aimerais un texte de {nombre_de_mots}, en incluant les titres des parties, les titres des sous parties et me donnant le nombre de mots de chaque partie. Vous devrez devrez essayer d'inclure celons les besoins un ou plusieurs [tableau], des [images], des [listes], des [liens internes], des [boutons], des [vidéos], etc..."
         elif prompt_option == "Géoloc IA":
             prompt_text = "Veuillez insérer ici votre propre texte de prompt pour l'option 2."
         else:
@@ -94,4 +95,48 @@ if uploaded_file and api_key_1 and api_key_2 and api_key_3:
     b64 = base64.b64encode(csv).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="resultat.csv">Télécharger les résultats en format CSV</a>'
     st.markdown(href, unsafe_allow_html=True)
+
+    # Deuxième itération pour générer un appel à OpenAI GPT-4 avec un prompt de rédaction
+    st.header("Génération de contenu avec OpenAI GPT-4")
+    
+    # Création d'un DataFrame pour stocker les résultats de la deuxième itération
+    result_data_2 = []
+
+    for index, row in df.iterrows():
+        structure_hn = row["Structure Hn suggerée"]
+
+        # Création du prompt en utilisant la structure Hn
+        prompt_text_2 = f"{structure_hn} Veuillez écrire un article basé sur cette structure."
+
+        messages_2 = [
+            {"role": "system", "content": prompt_text_2},
+        ]
+
+        if message:
+            messages_2.append(
+                {"role": "user", "content": message},
+            )
+            chat_2 = openai.ChatCompletion.create(
+                model="gpt-4", messages=messages_2
+            )
+            reply_2 = chat_2.choices[0].message.content
+
+            # Ajout des résultats dans le DataFrame
+            result_data_2.append({
+                "Structure Hn": structure_hn,
+                "Article généré": reply_2
+            })
+
+    # Conversion de la liste de résultats en DataFrame
+    df_2 = pd.DataFrame(result_data_2)
+
+    # Affichage du DataFrame dans l'application Streamlit
+    st.write(df_2)
+
+    # Bouton pour télécharger le fichier CSV résultant
+    csv_2 = df_2.to_csv(index=False, encoding="utf-8").encode()
+    b64_2 = base64.b64encode(csv_2).decode()
+    href_2 = f'<a href="data:file/csv;base64,{b64_2}" download="resultat_2.csv">Télécharger les résultats de la deuxième itération en format CSV</a>'
+    st.markdown(href_2, unsafe_allow_html=True)
+
 
